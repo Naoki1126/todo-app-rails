2746092885bbd13031b2f4f621660ec77f91db55
Naoki Maruyama <m.naoki85@gmail.com>
2021-09-05 19:03:59 +0900

    update to_do_lists

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'to_dos#index'
   resources :to_dos
+
+  namespace :api, { format: 'json'} do
+    namespace :v1 do
+      resources :to_dos
+    end
+  end
 end
